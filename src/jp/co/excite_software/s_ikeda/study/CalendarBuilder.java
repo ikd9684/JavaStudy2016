887b99d56e91fbaaa74617a2c937dafc0007cd9b
Shigehiro Ikeda <ikd9684@gmail.com>
2016-07-17 16:48:25 +0900

    直したつもりが全然ちがうもの上げてたので修正。

--- src/jp/co/excite_software/s_ikeda/study/CalendarBuilder.java
+++ src/jp/co/excite_software/s_ikeda/study/CalendarBuilder.java
@@ -2,161 +2,79 @@ package jp.co.excite_software.s_ikeda.study;
 
 import static java.util.Calendar.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.io.PrintStream;
 import java.util.GregorianCalendar;
 
 public class CalendarBuilder {
 
-    /**
-     * カレンダー出力処理を実装するためのインターフェイスです。
-     */
-    public interface CalendarOutput {
+    public static void main(String[] args) {
 
-        /**
-         * 年月の出力処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         * @param year 年
-         * @param month 月
-         */
-        public void outputYearAndMonth(PrintStream out, int year, int month);
+        CalendarBuilder builder = new CalendarBuilder(System.out);
 
-        /**
-         * 曜日ヘッダの出力処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         * @param dayOfWeek 曜日 {@link java.util.Calendar.DAY_OF_WEEK}
-         */
-        public void outputWeekHeader(PrintStream out, int dayOfWeek);
-
-        /**
-         * 日の出力処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         * @param year 年
-         * @param month 月
-         * @param dayOfMonth 日
-         * @param dayOfWeek 曜日 {@link java.util.Calendar.DAY_OF_WEEK}
-         * @param monthOffset 指定した月ならば 0、前の月なら -1 次の月なら 1
-         */
-        public void outputDate(PrintStream out, int year, int month, int dayOfMonth, int dayOfWeek, int monthOffset);
-
-        /**
-         * 月表示の開始処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         */
-        public void outputOpenMonth(PrintStream out);
-
-        /**
-         * 月表示の終了処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         */
-        public void outputCloseMonth(PrintStream out);
-
-        /**
-         * 週ヘッダ表示の開始処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         */
-        public void outputOpenWeekHeader(PrintStream out);
-
-        /**
-         * 週ヘッダ表示の終了処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         */
-        public void outputCloseWeekHeader(PrintStream out);
-
-        /**
-         * 週表示の開始処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         */
-        public void outputOpenWeek(PrintStream out);
-
-        /**
-         * 週表示の終了処理を実装してください。
-         * 
-         * @param out このPrintSreamに対して出力してください
-         */
-        public void outputCloseWeek(PrintStream out);
+        builder.outputCalendar(2016, 7);
+        builder.outputCalendar(2016, 8);
+        builder.outputCalendar(2016, 9);
     }
 
-    /** System.lineSeparator() */
-    private static final String LS = System.lineSeparator();
     /**  */
     private static final int[] WEEK = {
             SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY,
     };
+    private static final String [] WEEK_NAME = {
+            "日", "月", "火", "水", "木", "金", "土",
+    };
+
+    /**  */
+    private PrintStream out;
+
+    /**
+     * 
+     * @param out
+     */
+    public CalendarBuilder(PrintStream out) {
+        this.out = out;
+    }
 
     /**
      * 
      * @param year
      * @param month
-     * @param output
-     * @return
-     * @throws IOException 
      */
-    public static String build(final int year, final int month, CalendarOutput output) throws IOException {
+    public void outputCalendar(final int year, final int month) {
 
-        try (PipedInputStream pis = new PipedInputStream();
-             PipedOutputStream pos = new PipedOutputStream();
-             PrintStream out = new PrintStream(pos);) {
+        GregorianCalendar calendar = new GregorianCalendar (year, month - 1, 1);
+        calendar.add(DATE, 1 - calendar.get(DAY_OF_WEEK));
 
-            pis.connect(pos);
+        out.printf("%d年%d月", year, month).println();
 
-            output.outputYearAndMonth(out, year, month);
-            output.outputOpenWeekHeader(out);
-
-            for (int w : WEEK) {
-                output.outputWeekHeader(out, w);
-            }
-            output.outputCloseWeekHeader(out);
-
-            GregorianCalendar calendar = new GregorianCalendar (year, month - 1, 1);
-            calendar.add(DATE, 1 - calendar.get(DAY_OF_WEEK));
-
-            output.outputOpenMonth(out);
-
-            while (true) {
-                output.outputOpenWeek(out);
-
-                for (int w : WEEK) {
-                    int y = calendar.get(YEAR);
-                    int m = calendar.get(MONTH) + 1;
-                    int d = calendar.get(DAY_OF_MONTH);
-                    output.outputDate(out, y, m, d, w, m - month);
-
-                    calendar.add(DATE, 1);
-                }
-
-                output.outputCloseWeek(out);
-
-                if (calendar.get(MONTH) + 1 != month) {
-                    break;
-                }
-            }
-
-            output.outputCloseMonth(out);
-
-            out.close();
-
-            StringBuilder sb = new StringBuilder();
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(pis));) {
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    sb.append(line + LS);
-                }
-            }
-
-            return sb.toString();
+        for (int w : WEEK) {
+            out.printf(WEEK_NAME[w - 1]);
+            out.printf(w == SATURDAY ? "" : " ");
         }
+        out.println();
+
+        while (true) {
+            for (int w : WEEK) {
+                int m = calendar.get(MONTH) + 1;
+                int d = calendar.get(DAY_OF_MONTH);
+
+                if (m == month) {
+                    out.printf(w == SUNDAY ? "" : " ");
+                    out.printf("%2d", d);
+                }
+                else {
+                    out.printf(w == SUNDAY ? "  " : "   ");
+                }
+
+                calendar.add(DATE, 1);
+            }
+
+            out.println();
+
+            if (calendar.get(MONTH) + 1 != month) {
+                break;
+            }
+        }
+        out.println();
     }
 }
